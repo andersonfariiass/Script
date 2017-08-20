c7e9540747e86aabf9164fcc10d9e61454490937
andersonfariiass <31108276+andersonfariiass@users.noreply.github.com>
2017-08-20 12:13:51 -0300

    Update list_user.sh

--- Scripts_aula/list_user.sh
+++ Scripts_aula/list_user.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Script que informa a quaitidade de usuarios do sistema
 echo "Quantidade de usuarios do sistema"
-wc -l /etc/passwd
+wc -l /etc/passwd | cut -d: -f 1 ##Conta quantas linhas existe no arquivo /etc/passwd filtrando somente a primeira coluna.
 
 
