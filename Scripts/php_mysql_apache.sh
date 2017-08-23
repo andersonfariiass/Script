458800e0152375191d4ab028c819e5af41e3f1f8
andersonfariiass <31108276+andersonfariiass@users.noreply.github.com>
2017-08-23 10:47:15 -0300

    Update php_mysql_apache.sh

--- Scripts/php_mysql_apache.sh
+++ Scripts/php_mysql_apache.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-apt-get install apache2 mysql-server-5.0 mysql-server php5 php5-mysql
+apt-get install apache2 mysql-server-5.0 mysql-server php5 php5-mysql #Intala os pacotes apache2 mysql-server php5
 
 
 cd /etc/apache2/mods-enabled/
 ln -ns ../mods-available/php5.conf php5.conf
 ln -ns ../mods-available/php5.load php5.load
 
-/etc/init.d/apache2 reload
+/etc/init.d/apache2 reload #recarregando as configuracoes do apache
 
-cd /home/luis/Desktop/
 
 
 
