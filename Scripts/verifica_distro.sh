852584d64f2baa507c2aa0e5e66699357a168b4f
andersonfariiass <31108276+andersonfariiass@users.noreply.github.com>
2017-08-23 11:50:35 -0300

    Update verifica_distro.sh

--- Scripts/verifica_distro.sh
+++ Scripts/verifica_distro.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #Esse script verifica qual e a distro linux.
 
-if [ -f /etc/debian_version ] #verifica se o diretorio /etc/debain_version existe.
+if [ -f /etc/debian_version ] #verifica se o arquivo /etc/debain_version é um arquivo normal.
 then
   LINUX_DISTRO="Debian" #Se existir a variavel armazena a versao  da distro debian
 else
-  if [ -f /etc/redhat-release ] #Caso o primeiro if seja falso, verifica-se se existe o diretorio /etc/redhat-release
+  if [ -f /etc/redhat-release ] #Caso o primeiro if seja falso, verifica-se se o arquivo /etc/redhat-release é um arquivo normal
   then
       LINUX_DISTRO="Redhat" #Se verdadeiro a variavel armazena a versao da distro redhat
   else
-      if [ -f /etc/SuSE-release ] #Caso o teste anterior seja falso, testa se existe o diretorio /etc/SuE-release
+      if [ -f /etc/SuSE-release ] #Caso o teste anterior seja falso, testa se o arquivo /etc/SuE-release é um arquivo normal
       then
 	 LINUX_DISTRO="Suse" #Se verdadeido a variavel armazena aversao da distro Suse
       else
-	 LINUX_DISTRO="Other" #Caso nehum teste seja verdadeiro a variavel armazana um valor informando que a versao uma qualquer.
+	 LINUX_DISTRO="Other" #Caso nenhum teste seja verdadeiro a variavel armazana um valor informando que é uma outra versão.
       fi
    fi
 fi	
