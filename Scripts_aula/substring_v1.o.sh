6122dd0206374dc48b90a8f26e0c6c59bae5a2e0
andersonfariiass <anderson_si@outlook.com>
2017-08-29 14:05:44 -0300

    Add mensagem de erro!

--- Scripts_aula/substring_v1.o.sh
+++ Scripts_aula/substring_v1.o.sh
@@ -4,4 +4,4 @@
 str=$1
 str2=$2
 test $# -ne 2 && exit #Verifica se a quantidade de paramentros e maior que 2
-echo $str2 | grep -qs $str && echo "$str está contida em $str2" #verifica se na segunda palavra existe alguma ocorrencia da primeira e a exibe.
+echo $str2 | grep -qs $str && echo "$str está contida em $str2" || echo "$str nao esta contido em $str2" #verifica se na segunda palavra existe alguma ocorrencia da primeira e a exibe.
