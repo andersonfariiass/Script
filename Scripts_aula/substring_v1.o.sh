b31d0fa314ff760c5d526cd0a39c64602536cce2
andersonfariiass <anderson_si@outlook.com>
2017-08-29 13:58:45 -0300

    Add Comentários

--- Scripts_aula/substring_v1.o.sh
+++ Scripts_aula/substring_v1.o.sh
@@ -3,6 +3,5 @@
 #Recebe duas strings e verifica se há ocorrência da primera dentro da segunda
 str=$1
 str2=$2
-test $# -ne 2 && exit
-echo $str2 | grep -qs $str && echo "$str está contida em $str2"
-
+test $# -ne 2 && exit #Verifica se a quantidade de paramentros e maior que 2
+echo $str2 | grep -qs $str && echo "$str está contida em $str2" #verifica se na segunda palavra existe alguma ocorrencia da primeira e a exibe.
