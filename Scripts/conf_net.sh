4d50699cc3d6718316faf74429a89ffab803e1a0
andersonfariiass <anderson_si@outlook.com>
2017-09-01 22:23:04 -0300

    Edit/coment conf_net.sh

--- Scripts/conf_net.sh
+++ Scripts/conf_net.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #Script para configuracao da placa de rede local manual
 
-ifconfig
+ifconfig #Usado para listar as interfaces disponiveis
 echo
 
-read -p "Qual interface de rede deseja configurar? " INTF
+read -p "Qual interface de rede deseja configurar? " INTF #Seta qual interface será configurada
 echo
-read -p "Informe o IP destinado ao host EX(192.168.1.25): " IP
+read -p "Informe o IP destinado ao host EX(192.168.1.25): " IP #Armazena qual IP será atribuido a interface
 echo 
-read -p "Informe a mascara de rede: "  MASK
+read -p "Informe a mascara de rede EX(255.255.255.0): "  MASK #Mascara da rede
 echo
-read -p "Informe o GateWay da rede: " GW
+read -p "Informe o GateWay da rede: " GW #Gateway da rede
 echo
 sleep 1
 
@@ -19,19 +19,19 @@ clear
 echo "Configurando..."
 sleep 3
 
-ifconfig $INTF $IP netmask $MASK
+ifconfig $INTF $IP netmask $MASK #Seta as configurações desejadas
 
-echo "Testando o IP adicionado."
-ping -c2 $IP
+echo "Testando o IP adicionado." 
+ping -c2 $IP #Testa o IP com um ping
 sleep 2
 
 clear
 
-if [ $? -eq 0 ]; then
-	route add default gw $GW ; 
+if [ $? -eq 0 ]; then #O $? pega o ultimo resultado do ping e verifica se foi igual a zero, se sim o ping ocorreu com sucesso.
+	route add default gw $GW ; #Add a rota default
 	echo "Testando GateWay..."
-	ping -c2 $GW
-	if [ $? -eq 0 ]; then
+	ping -c2 $GW #Testa o GW com o ping
+	if [ $? -eq 0 ]; then 
 		echo "Rede configurada com sucesso!"
 		sleep 3
 		clear
