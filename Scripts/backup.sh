1e9eb310cf4d79a428d3daa82593ea97d7a5fa27
andersonfariiass <anderson_si@outlook.com>
2017-09-06 20:55:59 -0300

    Create backup.sh

---
+++ Scripts/backup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#Script para fazer backup do diretorio especificado pelo usuario com nome do arquivo "backup + date + hora"
+
+read -p "Qual o diretório a ser feito backup: " DIR
+echo
+
+DATA=`date +%Y-%m-%d-%H:%M`
+cd /home/
+tar -zcvf backup-$DATA.tar.gz $DIR
+#cd /tmp/
+NOME=backup-$DATA.tar.gz
+echo "$NOME"
+
+#if [ -e $NOME ]; then
+#	echo "Backup criado com Sucesso!"
+#	sleep 2
+#	exit
+#else
+#	echo "ERRO!"
+#fi
+
