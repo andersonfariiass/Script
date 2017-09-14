7014c216dd1365dc4e59643cbe28daee2684b5ee
andersonfariiass <anderson_si@outlook.com>
2017-09-13 21:16:06 -0300

    Edit teste-conectividade-for.sh

--- Scripts/teste-conectividade-for.sh
+++ Scripts/teste-conectividade-for.sh
@@ -8,7 +8,7 @@ for IP in `cat servidores.txt`
 do
 
 RETURN=`ping -q -c1 $IP | grep received | cut -d " " -f 4`
-	if [ $RETURN == 1 ]; then
+	if [ $RETURN == 1 ]; then #usa o == porque o 1 é uma string, pois a saida do ping e um texto
 		echo "Teste do $IP com sucesso" >> $ARQ
 	else
 		echo "Teste do $IP falhou" >> $ARQ
