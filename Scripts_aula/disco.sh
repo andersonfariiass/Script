4812828e4d7e9e12a4f5b549c642fc3ea2437635
andersonfariiass <anderson_si@outlook.com>
2017-09-22 09:32:34 -0300

    Edit Script_aula/disco.sh

--- Scripts_aula/disco.sh
+++ Scripts_aula/disco.sh
@@ -1,10 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 #Script que verifica o uso do disco
 echo "Uso do disco"
 
-df -h > tmp.txt #Joga a saida do df para o arquivo tmp.txt
-head -n1 tmp.txt && grep /dev/sd tmp.txt #Primeiro exibi a primeira linha e as linhas que contem as unidades /dev/sd 
+DISCO=$(df -h | tr -s " " | grep -i /dev)
+disk=$(df -h | tr -s " " | grep -i /home)
+echo $DISCO
 
-rm -r tmp.txt #Apaga o arquivo
-echo
+if [ -z $disk ]; then
+	echo "O diretorio /home nao esta em particao separada!"
+	echo "$DISCO"
+else
+	echo "Uso da particao /home."
+	echo "$disk"
+fi
+
+#df -h > tmp.txt #Joga a saida do df para o arquivo tmp.txt
+#head -n1 tmp.txt && grep /dev/sd tmp.txt #Primeiro exibi a primeira linha e as linhas que contem as unidades /dev/sd 
+
+#rm -r tmp.txt #Apaga o arquivo
+#echo
 
