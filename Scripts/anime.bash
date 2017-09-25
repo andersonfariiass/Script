1ed41c2c4317e6d0e083f189de5da12ef7d02e2b
andersonfariiass <anderson_si@outlook.com>
2017-09-25 15:53:23 -0300

    Create anime.bash

---
+++ Scripts/anime.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+function Anime1(){ 
+    local i=2 
+    while [[ ! -z $(ps | grep "$!") ]]; do  
+        printf "Processando [          ]" | sed "s/ /\-\>/$i" 
+        printf "\r" 
+        sleep 0.05 
+        ((i++))
+        if [ "$i" == 11 ]; then 
+            for ((i;i>2;i--)); do 
+                printf "Processando [          ]" | sed "s/ /\<\-/$i"
+                printf "\r" 
+                sleep 0.05
+            done
+        fi
+    done
+
+printf "Processando [---feito---]\n"
+}
+
+function Anime2(){
+
+    spinner=(\\ \| / -)
+    while [[ ! -z $(ps | grep "$!") ]]; do
+        i=0
+        for ((i;i<4;i++)); do
+            printf "Processando [${spinner[i]}]"
+            sleep 0.20
+            printf "\r"
+        done
+    done                
+printf "Processando [*]\n"
+}
+
+
+sleep 3 & Anime1
+sleep 3 & Anime2
