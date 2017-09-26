64908a8f70c9fe5d79b87f8d79b93b73a731ffaa
andersonfariiass <anderson_si@outlook.com>
2017-09-25 23:51:20 -0300

    Create exemplo.bash

--- Scripts/exemplo.bash
+++ Scripts/exemplo.bash
@@ -13,12 +13,59 @@ echo "2 - Particionar disco"
 echo "3 - Montar/Desmontar CDROM"
 echo "4 - Sair"
 
-read OP
+read -s OP
 
 if [ "$OP" == 1 ]; then
 	clear
-	echo "#----FileSustem----#" 
-	$(df -h | grep -e /sd -e /hd)
+	echo "#----FileSystem----#" 
+	df -h | head -n1
+	df -h | grep -e /hd  -e /sd
 fi
 
+#Executa o cfdisk
+if [ "$OP" == 2 ]; then
+	clear
+	sudo cfdisk
+fi
 
+if [ "$OP" == 3 ]; then
+	clear
+	echo
+	echo "1 - Montar CDROM"
+	echo "2 - Desmontar o CDROM"
+	read CMD
+	
+	if [ "$CMD" == 1 ]; then
+		mount /dev/cdrom
+		clear
+		echo "CDROM Montado!"
+	fi
+	if [ "$CMD" == 2 ]; then
+		umount /dev/cdrom
+		clear
+		echo "CDROM desmontado!"
+	fi
+	#Controle de saida do utilitário
+	if [ $CMD != 1 ];then
+		if [ $CMD != 2 ]; then
+			clear
+			echo "Comando Inválido!"
+		fi
+	fi
+fi
+if [ "$OP" == 4 ]; then
+	clear 
+	exit
+fi
+
+#Controle de erro
+if [ "$OP" != 1 ]; then
+	if [ "$OP" != 2 ]; then
+		if [ "$OP" != 3 ]; then
+			if [ "$OP" != 4 ]; then
+				clear
+				echo "Comando Inválido!"
+			fi
+		fi
+	fi
+fi
