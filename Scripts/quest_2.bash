ca8bdeb8dc18a390e6470a742b7f8b3cc919af57
andersonfariiass <anderson_si@outlook.com>
2017-09-27 19:52:16 -0300

    Edit quest 2

--- Scripts/quest_2.bash
+++ Scripts/quest_2.bash
@@ -11,21 +11,21 @@ echo "0 - Sair"
 
 read -p "Selecione uma das opções lista para exibir as informações: " OP
 
-if [ $OP -eq 1 ]; then
+if [ "$OP" = 1 ]; then
 	echo "#----Informações sobre Memória---#"
 	MEM=$(cat /proc/meminfo | grep "Mem")
 	#MEM=$(cat /proc/meminfo | grep  "Mem" | cut -d " " -f 2)
 	echo "$MEM"
-elif [ $OP -eq 2 ]; then
+elif [ "$OP" = 2 ]; then
 	echo "#----Informações sobre Processador----#"
 	NCPU=$(cat /proc/cpuinfo | grep  name | tail -1 | cut -d\: -f 1-3)
 	echo "$NCPU"
-elif [ $OP -eq 3 ]; then
+elif [ "$OP" = 3 ]; then
 	echo "#----Informações sobre Discos de Armazenamento---#"
 	DISK=$(df -h | grep -e /sd -e /hd | tr -s '')
 	echo "$(df -h | head -n1 | tr -s '')"
 	echo "$DISK"
-elif [ $OP -eq 0 ];  then
+elif [ "$OP" = 0 ];  then
 	echo "Saindo..."
 	sleep 1
 	clear
