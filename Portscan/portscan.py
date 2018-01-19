b5aaba743e235fa52eb9f8a98904ca69a524863f
andersonfariiass <anderson_si@outlook.com>
2018-01-19 09:44:21 -0500

    Curso Pentest - portscan.py

--- Portscan/portscan.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('bancocn.com', 80))
-client.send('teste \n\n')
-reposta = client.recv(1024)
-
-print reposta
